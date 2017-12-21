b27cbe163d49146f074715d04853ec463814de17
Binguh <32099174+Binguh@users.noreply.github.com>
2017-12-21 14:31:22 +0100

    Initial commit

---
+++ view/template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title><?= $title; ?></title>
+	</head>
+
+	<header>
+		<?= $headerContent; ?>
+	</header>
+
+	<body>
+		<?= $bodyContent; ?>
+	</body>
+
+	<footer>
+		<?= $footerContent; ?>
+	</footer>
+
+</html>
