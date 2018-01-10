273ba791594f9a70852082bffa20a11bfd11834b
Binguh <32099174+Binguh@users.noreply.github.com>
2018-01-10 10:31:37 +0100

    update game sheet

--- view/backend/updateGameSheetView.php
+++ view/backend/updateGameSheetView.php
@@ -10,26 +10,26 @@
 <section>
 	<form action="index.php?action=updateGameSheet&amp;id=<?= $gameData['id']; ?>" method="post">
 		<div>
-			<input type="text" name="name" id="gameName" placeholder="Nom du jeu" value="<?= $gameData['gameName']; ?>">
+			<input type="text" name="name" id="gameName" placeholder="Nom du jeu" value="<?= $gameData['gameName'] ?>">
 			<label for="gameName">Nom du jeu</label>
 		</div>
 		<div>
 			<fieldset>
   				<legend>Type de coop</legend>
 				<div>
-					<input type="checkbox" id="localCoop" name="coopType" value="localCoop">
+					<input type="radio" id="localCoop" name="coopType" value="localCoop">
 					<label for="localCoop">Local</label>
 				</div>
 				<div>
-					<input type="checkbox" id="onlineCoop" name="coopType" value="onlineCoop">
+					<input type="radio" id="onlineCoop" name="coopType" value="onlineCoop">
 					<label for="onlineCoop">En ligne</label>
 				</div>
 				<div>
-					<input type="checkbox" id="localOrOnlineCoop" name="coopType" value="localOrOnlineCoop">
+					<input type="radio" id="localOrOnlineCoop" name="coopType" value="localOrOnlineCoop">
 					<label for="localOrOnlineCoop">Local ou en ligne</label>
 				</div>
 				<div>
-					<input type="checkbox" id="localAndOnlineCoop" name="coopType" value="localAndOnlineCoop">
+					<input type="radio" id="localAndOnlineCoop" name="coopType" value="localAndOnlineCoop">
 					<label for="localAndOnlineCoop">Local et en ligne</label>
 				</div>
 			</fieldset>
@@ -38,15 +38,19 @@
 			<fieldset>
 				<legend>Microtransaction</legend>
 				<div>
-					<input type="checkbox" id="lootbox" name="microLoot" value="lootbox">
+					<input type="radio" id="nope" name="microLoot" value="lootbox" checked>
+					<label for="lootbox">Aucunes microtransactions</label>
+				</div>
+				<div>
+					<input type="radio" id="lootbox" name="microLoot" value="lootbox">
 					<label for="lootbox">Lootbox</label>
 				</div>
 				<div>
-					<input type="checkbox" id="microtransaction" name="microLoot" value="microtransaction">
+					<input type="radio" id="microtransaction" name="microLoot" value="microtransaction">
 					<label for="microtransaction">Microtransaction</label>
 				</div>
 				<div>
-					<input type="checkbox" id="lootboxAndMicro" name="microLoot" value="lootboxAndMicro">
+					<input type="radio" id="lootboxAndMicro" name="microLoot" value="lootboxAndMicro">
 					<label for="lootboxAndMicro">Lootbox et microtransaction</label>
 				</div>
 			</fieldset>
@@ -56,23 +60,27 @@
 			<label for="gameMinPlayerNumber">Nombre minimum de joueurs</label>
 		</div>
 		<div>
-			<input type="number" name="maxPlayerNumber" id="gameMaxPlayerNumber" value="<?= $gameData['gameMaxPlayerNumber']; ?>">
+			<input type="number" name="maxPlayerNumber" id="gameMaxPlayerNumber" value="<?= $gameData['gameMaxPlayerNumber'] ?>">
 			<label for="gameMaxPlayerNumber">Nombre maximum de joueurs</label>
 		</div>
 		<div>
-			<input type="date" name="releaseDate" id="gameReleaseDate">
+			<input type="date" name="releaseDate" id="gameReleaseDate" value="<?= $gameData['gameReleaseDate'] ?>">
 			<label for="gameReleaseDate">Date de sortie</label>
 		</div>
 		<div>
-			<input type="number" name="price" id="gamePrice">
+			<input type="number" name="price" id="gamePrice" value="<?= $gameData['gamePrice'] ?>">
 			<label for="gamePrice">Prix</label>
 		</div>
 		<div>
-			<input type="text" name="officialWebsite" id="gameOfficialWebsite">
+			<input type="text" name="officialWebsite" id="gameOfficialWebsite" value="<?= $gameData['gameOfficialWebsite'] ?>">
 			<label for="gameOfficialWebsite">Site officiel</label>
 		</div>
 		<div>
-			<button>Ajouter</button>
+			<input type="file" name="gameSheetImage" id="gameSheetImage">
+			<label for="gameSheetImage">Image</label>
+		</div>
+		<div>
+			<button>Modifier</button>
 		</div>
 	</form>
 </section>
